77f7479b3752e260b19acb83c5be6b4435a398cd
Maxime-syst <maxime.genoux@icloud.com>
2025-11-11 08:59:27 +0100

    11.11.25
    11.11.25

---
+++ 2_web/js/exemple.js
@@ -0,0 +1,24 @@
+//console.log(message);
+
+var message = "Bienvenu";
+
+//function ma_fonction() {
+//   console.log("ma fonction");
+//}
+
+//ma_fonction();
+
+//console.log(document.querySelector (".Important").textContent)
+console.log(document.getElementById("Bienvenu").textContent)
+//for(var i = 0; i<10 ; i++ ){
+//   console.log(i);
+//}
+
+function ajouter_perso(){
+    var  mon_perso = "Jean";
+
+    var nouveau_li = document.createElement(li);
+    var noueau_text = document.createTextNode(text_perso)
+
+    console.log("perso")
+}
