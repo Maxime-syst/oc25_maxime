00edbebb348194333fc4d95885435baf17b7706d
Maxime-syst <maxime.genoux@icloud.com>
2025-11-13 15:19:04 +0100

    13.11.25

--- 2_web/js/exemple.js
+++ 2_web/js/exemple.js
@@ -1,24 +1,60 @@
-//console.log(message);
+var  jean ={
+    nom: "jean" ,
+    classe: "guerrier" ,
+    niveau:2,
+    passer_niveau: function() {
+        this.niveau++;
+    }
+}
 
-var message = "Bienvenu";
+console.log(jean);
+console.log(jean.classe)
+jean.passer_niveau();
+console.log(jean.niveau)
 
-//function ma_fonction() {
-//   console.log("ma fonction");
-//}
+function creer_personnage(nom,classe,niveau){
+    var nouveau_personnage ={
+        nom: nom,
+        classe: classe,
+        niveau: niveau,
+        passer_niveau: function(){
+            this.niveau++;
+        },
+        creer_li: function(){
+            var li_personnage = document.createElement("li");
+            var  texte_personnage = document.createTextNode(
+                this.nom + "(" +  this.classe + ", niveau" + this.niveau + ")"
+            );
+            li_personnage.appendChild(texte_personnage);
+            li_personnage.setAttribute("class", this.classe)
+            return li_personnage
+        }
 
-//ma_fonction();
+        
+    }
+    return nouveau_personnage
+}
 
-//console.log(document.querySelector (".Important").textContent)
-console.log(document.getElementById("Bienvenu").textContent)
-//for(var i = 0; i<10 ; i++ ){
-//   console.log(i);
-//}
+var nicole = creer_personnage("Nicole","voleur",3);
 
-function ajouter_perso(){
-    var  mon_perso = "Jean";
+console.log(nicole)
 
-    var nouveau_li = document.createElement(li);
-    var noueau_text = document.createTextNode(text_perso)
+var troupe = [  
+    creer_personnage("Jean","guerrier", 2 ),
+    creer_personnage("Nicole","voleur", 3 ),
+    creer_personnage("Matteo", "magicien", 1 ),
+    creer_personnage("Timo","guerrier", 2 ),
+    creer_personnage("Mélanie","voleur", 3 ),
+    creer_personnage("Hanyang", "farfadet", 12 )
+]
 
-    console.log("perso")
+console.log(troupe); 
+console.log(troupe[0])
+
+for (var i=0; i< troupe.length; i++ ){
+    var perso = troupe[i];
+    var li_personnage= perso.creer_li();
+
+    var liste_perso = document.getElementById("liste_de_perso");
+    liste_perso.appendChild(li_personnage);
 }
