17277c5045bdc3f5183ac83766e8178aac365a03
Maxime-syst <maxime.genoux@icloud.com>
2025-11-18 09:01:05 +0100

    18.11

---
+++ 2_web/js/exemple3.js
@@ -0,0 +1,16 @@
+console.log("Bonjour");
+
+
+const statut = document.getElementById("statut");
+const ctx_statut = statut.getContext("2d");
+
+ctx_statut.fillStyle="green";
+ctx_statut.fillRect(0,0,300,300);
+
+const dessin = document.getElementById("dessin");
+const context = dessin.getContext("2d");
+
+context.fillStyle="black";
+context.fillRect(0,0,100,100);
+context.fillRect(100,100,100,100);
+context.fillRect(200,200,100,100);
